01c6457e56abe7f8a01bdb01de42b5aa65577fe7
Pawan <pawandeepk@my.com>
2021-11-30 16:57:17 +0000

    created first endpoint

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const app = express();
 // Configure middleware -------------------------
 
 // Configure CRUDL endpoints --------------------
+app.get('/', (req,res) => {
+    res.send("Hello World");
+});
 
 // Start server ---------------------------------
 const PORT = process.env.PORT || 5000;
