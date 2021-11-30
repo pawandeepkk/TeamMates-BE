6b9c860677f329de5afb8bc55697f89f96b94372
Pawan <pawandeepk@my.com>
2021-11-30 17:29:22 +0000

    Configured a second "read" endpoint

--- app.js
+++ app.js
@@ -10,7 +10,20 @@ import { tableOfModules } from './data/tableOfModules.js';
 // Configure CRUDL endpoints --------------------
 // List all modules
 app.get('/api/modules', (req, res) => {
+    // validate request
+    // access data model
+    // response to request
     res.json(tableOfModules);
+});
+
+// Read specific record
+app.get('/api/modules/:id', (req, res) => {
+    // validate request
+    // access data model
+    const module = tableOfModules.find((module) => module.ModuleID === parseInt(req.params.id));
+    if (!module) res.status(404).json({ Message: `Record ${req.params.id} not found`});
+    // response to request
+    res.json(module);
 });
 
 // Start server ---------------------------------
